d84b8e545e37b4e3b1ad6a3cf3b059b603db8925
Alice Last <yydota@gmail.com>
2019-12-28 08:35:55 +0800

    see changelog 12.28

--- scripts/vscripts/abilities/abilitymomiji.lua
+++ scripts/vscripts/abilities/abilitymomiji.lua
@@ -1,133 +1,123 @@
 function OnMomiji01Spawn(keys)
-	local caster = EntIndexToHScript(keys.caster_entindex)
-	if caster.ability_momiji01_Spawn_unit == nil then
-		caster.ability_momiji01_Spawn_unit = {}
-	end
+    local caster = EntIndexToHScript(keys.caster_entindex)
+    if caster.ability_momiji01_Spawn_unit == nil then
+        caster.ability_momiji01_Spawn_unit = {}
+    end
 
-	count = 2 + FindTelentValue(caster,"special_bonus_unique_bounty_hunter")
+    count = 2 + FindTelentValue(caster, "special_bonus_unique_bounty_hunter")
 
-	for i=1,count do
-		local unit = CreateUnitByName(
-			"ability_momiji_Spawn_unit"
-			,caster:GetOrigin() + ( caster:GetForwardVector() + Vector(math.cos((i-1.5)*math.pi/3),math.sin((i-1.5)*math.pi/3),0) ) * 100
-			,false
-			,caster
-			,caster
-			,caster:GetTeam()
-		)
+    for i = 1, count do
+        local unit = CreateUnitByName("ability_momiji_Spawn_unit",
+                                      caster:GetOrigin() +
+                                          (caster:GetForwardVector() +
+                                              Vector(
+                                                  math.cos(
+                                                      (i - 1.5) * math.pi / 3),
+                                                  math.sin(
+                                                      (i - 1.5) * math.pi / 3),
+                                                  0)) * 100, false, caster,
+                                      caster, caster:GetTeam())
 
-		if unit == nil then return end
+        if unit == nil then return end
 
-		unit:CreatureLevelUp(keys.ability:GetLevel())
-		unit:SetControllableByPlayer(caster:GetPlayerOwnerID(), true) 
-		SetTargetToTraversable(unit)
+        unit:CreatureLevelUp(keys.ability:GetLevel())
+        unit:SetControllableByPlayer(caster:GetPlayerOwnerID(), true)
+        SetTargetToTraversable(unit)
 
-		unit:AddAbility("ability_system_criticalstrike")
-		local ability = unit:FindAbilityByName("ability_system_criticalstrike")
-		ability:SetLevel(1)	
-		if FindTelentValue(caster,"special_bonus_unique_lycan_1") == 1 then 
-			
-			unit:RemoveAbility("ability_system_criticalstrike")
-			unit:RemoveModifierByName("modifier_system_attack")
-			unit:AddAbility("ability_system_criticalstrike_2")
-			ability = unit:FindAbilityByName("ability_system_criticalstrike_2")
-			ability:SetLevel(1)
+        unit:AddAbility("ability_thdots_momijiex")
+        local ability = unit:FindAbilityByName("ability_thdots_momijiex")
+        ability:SetLevel(1)
 
-			caster:RemoveAbility("ability_system_criticalstrike")
-			caster:RemoveModifierByName("modifier_system_attack")
-			caster:AddAbility("ability_system_criticalstrike_2")
-			ability = caster:FindAbilityByName("ability_system_criticalstrike_2")
-			ability:SetLevel(1)
-		end
+        if FindTelentValue(caster, "special_bonus_unique_bounty_hunter_2") == 1 then
+            unit:AddAbility("ability_thdots_momiji_unit")
+            unit:SetBaseMaxHealth(1800)
+            ability = unit:FindAbilityByName("ability_thdots_momiji_unit")
+            ability:SetLevel(1)
+        end
 
-		if FindTelentValue(caster,"special_bonus_unique_bounty_hunter_2") == 1 then 
-			unit:AddAbility("ability_thdots_momiji_unit")
-			unit:SetBaseMaxHealth(1800)
-			ability = unit:FindAbilityByName("ability_thdots_momiji_unit")		
-			ability:SetLevel(1)
-		end
+        if keys.ability:GetLevel() >= 3 then
+            unit:AddAbility("lycan_summon_wolves_invisibility")
+            ability = unit:FindAbilityByName("lycan_summon_wolves_invisibility")
+            ability:SetLevel(1)
+        end
 
-		if keys.ability:GetLevel() >=3 then
-			unit:AddAbility("lycan_summon_wolves_invisibility")
-			ability = unit:FindAbilityByName("lycan_summon_wolves_invisibility")
-			ability:SetLevel(1)
-		end
+        local oldSwpanUnit = caster.ability_momiji01_Spawn_unit[i]
+        if oldSwpanUnit ~= nil and oldSwpanUnit:IsNull() == false then
+            oldSwpanUnit:ForceKill(false)
+        end
+        caster.ability_momiji01_Spawn_unit[i] = unit
 
-		local oldSwpanUnit = caster.ability_momiji01_Spawn_unit[i]
-		if oldSwpanUnit ~=nil and oldSwpanUnit:IsNull() == false then 
-			oldSwpanUnit:ForceKill(false)
-		end
-		caster.ability_momiji01_Spawn_unit[i] = unit
-
-		unit:SetContextThink(
-		"ability_momiji01_Spawn_unit_regen", 
-			function ()
-				if GameRules:IsGamePaused() then return 0.03 end
-				if GetDistanceBetweenTwoVec2D(caster:GetOrigin(),unit:GetOrigin()) <= keys.AuraDistance then
-					unit:Heal(keys.RegenAmount/5, caster)
-					local abilityMomiji04Caster = caster:FindAbilityByName("ability_thdots_momiji04")
-					if abilityMomiji04Caster ~= nil then
-						local abilityMomiji04 = unit:FindAbilityByName("ability_thdots_momiji04")
-						if abilityMomiji04==nil then
-							unit:AddAbility("ability_thdots_momiji04")
-							abilityMomiji04 = unit:FindAbilityByName("ability_thdots_momiji04")
-							abilityMomiji04:SetLevel(abilityMomiji04Caster:GetLevel())
-						elseif abilityMomiji04:GetLevel() ~= abilityMomiji04Caster:GetLevel() then
-							abilityMomiji04:SetLevel(abilityMomiji04Caster:GetLevel())
-						end
-					end
-				else
-					local abilityMomiji04 = unit:FindAbilityByName("ability_thdots_momiji04")
-					if abilityMomiji04 ~= nil then
-						unit:RemoveAbility("ability_thdots_momiji04")
-						unit:RemoveModifierByName("passive_momiji04_bonus")
-					end
-				end
-				return 0.2
-			end, 
-		0.2)
-	end
+        unit:SetContextThink("ability_momiji01_Spawn_unit_regen", function()
+            if GameRules:IsGamePaused() then return 0.03 end
+            if GetDistanceBetweenTwoVec2D(caster:GetOrigin(), unit:GetOrigin()) <=
+                keys.AuraDistance then
+                unit:Heal(keys.RegenAmount / 5, caster)
+                local abilityMomiji04Caster =
+                    caster:FindAbilityByName("ability_thdots_momiji04")
+                if abilityMomiji04Caster ~= nil then
+                    local abilityMomiji04 =
+                        unit:FindAbilityByName("ability_thdots_momiji04")
+                    if abilityMomiji04 == nil then
+                        unit:AddAbility("ability_thdots_momiji04")
+                        abilityMomiji04 =
+                            unit:FindAbilityByName("ability_thdots_momiji04")
+                        abilityMomiji04:SetLevel(
+                            abilityMomiji04Caster:GetLevel())
+                    elseif abilityMomiji04:GetLevel() ~=
+                        abilityMomiji04Caster:GetLevel() then
+                        abilityMomiji04:SetLevel(
+                            abilityMomiji04Caster:GetLevel())
+                    end
+                end
+            else
+                local abilityMomiji04 = unit:FindAbilityByName(
+                                            "ability_thdots_momiji04")
+                if abilityMomiji04 ~= nil then
+                    unit:RemoveAbility("ability_thdots_momiji04")
+                    unit:RemoveModifierByName("passive_momiji04_bonus")
+                end
+            end
+            return 0.2
+        end, 0.2)
+    end
 end
 
 function OnMomiji02Start(keys)
-	local caster = EntIndexToHScript(keys.caster_entindex)
-	local Duration = keys.Duration
-	AddFOWViewer( caster:GetTeam(), keys.target_points[1], keys.Radius, Duration, false)
-	THDReduceCooldown(keys.ability,FindTelentValue(caster,"special_bonus_unique_bounty_hunter_3"))
-	
-	local effectIndex = ParticleManager:CreateParticle("particles/econ/courier/courier_trail_05/courier_trail_05.vpcf", PATTACH_CUSTOMORIGIN, caster)
-	ParticleManager:SetParticleControl(effectIndex, 0, keys.target_points[1])
+    local caster = EntIndexToHScript(keys.caster_entindex)
+    local Duration = keys.Duration
+    AddFOWViewer(caster:GetTeam(), keys.target_points[1], keys.Radius, Duration,
+                 false)
+    THDReduceCooldown(keys.ability, FindTelentValue(caster,
+                                                    "special_bonus_unique_bounty_hunter_3"))
 
-	ParticleManager:DestroyParticleSystemTime(effectIndex,Duration)
+    local effectIndex = ParticleManager:CreateParticle(
+                            "particles/econ/courier/courier_trail_05/courier_trail_05.vpcf",
+                            PATTACH_CUSTOMORIGIN, caster)
+    ParticleManager:SetParticleControl(effectIndex, 0, keys.target_points[1])
 
-	local unit = CreateUnitByName(
-		"npc_vision_momiji_dummy_unit"
-		,caster:GetOrigin()
-		,false
-		,caster
-		,caster
-		,caster:GetTeam()
-	)
+    ParticleManager:DestroyParticleSystemTime(effectIndex, Duration)
 
-	unit:SetOrigin(keys.target_points[1])
+    local unit = CreateUnitByName("npc_vision_momiji_dummy_unit",
+                                  caster:GetOrigin(), false, caster, caster,
+                                  caster:GetTeam())
 
-	local abilityGEM = unit:FindAbilityByName("ability_thdots_momiji02_unit")
-	if abilityGEM ~= nil then
-		abilityGEM:SetLevel(keys.ability:GetLevel())
-		unit:CastAbilityImmediately(abilityGEM, 0)
-	end
+    unit:SetOrigin(keys.target_points[1])
 
-	local ability_dummy_unit = unit:FindAbilityByName("ability_dummy_unit")
-	ability_dummy_unit:SetLevel(1)
+    local abilityGEM = unit:FindAbilityByName("ability_thdots_momiji02_unit")
+    if abilityGEM ~= nil then
+        abilityGEM:SetLevel(keys.ability:GetLevel())
+        unit:CastAbilityImmediately(abilityGEM, 0)
+    end
 
-	unit:SetContextThink("ability_momiji_02_vision", 
-		function ()
-			if GameRules:IsGamePaused() then return 0.03 end
-			unit:RemoveSelf()
-		end, 
-	Duration)
+    local ability_dummy_unit = unit:FindAbilityByName("ability_dummy_unit")
+    ability_dummy_unit:SetLevel(1)
 
-	--[[Timer.Loop 'ability_momiji02_vision' (1.0, 150,
+    unit:SetContextThink("ability_momiji_02_vision", function()
+        if GameRules:IsGamePaused() then return 0.03 end
+        unit:RemoveSelf()
+    end, Duration)
+
+    --[[Timer.Loop 'ability_momiji02_vision' (1.0, 150,
 			function (i)
 				local targets = FindUnitsInRadius(
 					   caster:GetTeam(),		--caster team
@@ -143,27 +133,29 @@ function OnMomiji02Start(keys)
 					keys.ability:ApplyDataDrivenModifier( caster, v, "modifier_momiji02_antiinvisible", {Duration=1.0} )
 				end
 			end
-	)]]--
+	)]] --
 end
 
 function OnMomiji03Start(keys)
-	if is_spell_blocked(keys.target) then return end
-	local caster = EntIndexToHScript(keys.caster_entindex)
-	local target = keys.target
-	UtilStun:UnitStunTarget( caster,target,keys.Duration)
-	
-	local effectIndex = ParticleManager:CreateParticle("particles/units/heroes/hero_sven/sven_storm_bolt_projectile_explosion.vpcf", PATTACH_CUSTOMORIGIN, caster)
-	ParticleManager:SetParticleControl(effectIndex, 0, target:GetOrigin())
-	ParticleManager:SetParticleControl(effectIndex, 1, target:GetOrigin())
-	ParticleManager:SetParticleControl(effectIndex, 3, target:GetOrigin())
+    if is_spell_blocked(keys.target) then return end
+    local caster = EntIndexToHScript(keys.caster_entindex)
+    local target = keys.target
+    UtilStun:UnitStunTarget(caster, target, keys.Duration)
 
-	local DamageTable = {
-			victim = target, 
-			attacker = caster, 
-			ability = keys.ability,
-			damage = keys.ability:GetAbilityDamage(), 
-			damage_type = keys.ability:GetAbilityDamageType(), 
-			damage_flags = keys.ability:GetAbilityTargetFlags()
-	}
-	 UnitDamageTarget(DamageTable)
+    local effectIndex = ParticleManager:CreateParticle(
+                            "particles/units/heroes/hero_sven/sven_storm_bolt_projectile_explosion.vpcf",
+                            PATTACH_CUSTOMORIGIN, caster)
+    ParticleManager:SetParticleControl(effectIndex, 0, target:GetOrigin())
+    ParticleManager:SetParticleControl(effectIndex, 1, target:GetOrigin())
+    ParticleManager:SetParticleControl(effectIndex, 3, target:GetOrigin())
+
+    local DamageTable = {
+        victim = target,
+        attacker = caster,
+        ability = keys.ability,
+        damage = keys.ability:GetAbilityDamage(),
+        damage_type = keys.ability:GetAbilityDamageType(),
+        damage_flags = keys.ability:GetAbilityTargetFlags()
+    }
+    UnitDamageTarget(DamageTable)
 end
